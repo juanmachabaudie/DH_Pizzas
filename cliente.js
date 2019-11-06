d9367fbee1f4076762da47463db8df2949a4889a
juanmachabaudie <jumachabaudie@gmail.com>
2019-11-06 17:19:05 -0300

    Primer commit de clientes.js

---
+++ cliente.js
@@ -0,0 +1,50 @@
+const inquirer = require("inquirer");
+
+inquirer.prompt([
+      {
+          type: "confirm",
+          name: "paraLlevar",
+          message:"Es para delivery?",
+      },
+      {
+          type:"input",
+          name:"direccion",
+          message:"Ingrese su Dirección"
+      },
+      {
+          type: "input",
+          name: "NombreCliente",
+          message: "Escriba su Nombre",
+      },
+      {
+          type:"input",
+          name: "Telefono",
+          message:"Escriba su telefono"
+      },
+      {
+          type:"checkbox",
+          name:"gustosDePizza",
+          message:"Elija el gusto de la pizza",
+          choices:["Muzza","Jamon","Napolitana","A Caballo"]
+      },
+      {
+          type: "list",
+          name: "tamaño",
+          message:"Elija el Tamaño de la Pizza",
+          choices:["Small","Medium","Large"]
+      },
+      {
+          type:"list",
+          name:"bebida",
+          message:"Elija la Bebida",
+          choices:["CocaCola","Sprite","Aquarius","BonAqua"]
+      },
+      {
+          type:"checkbox",
+          name:"empanadas",
+          message:"Qué Gusto de Empanadas quiere?",
+          choices:["Carne Suave","Pollo","Jamon&Queso","Atun"]
+      }
+  ]).then(function (respuestas) {
+    console.log(respuestas);
+  })
